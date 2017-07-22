a821372cc95117942aefea5560d035577aef65fb
星野斉 <hhoshino-main@memomad.jp>
2017-07-22 11:49:56 +0900

    ソート前データを乱数で生成するように修正。乱数発生を1000に設定。

--- bublesort/main.cpp
+++ bublesort/main.cpp
@@ -1,55 +1,54 @@
 #include <iostream>
 
+#include <random>
+
 //関数プロトタイプ宣言
-void BubleSort(int *A, int N);
+void BubleSort(long *A, int N);
 
 int main() {
-    //std::cout << "Hello, World!" << std::endl;
-    int* A;
-    int length = 10;
+    long* A;
+    int length = 100;
 
-    A = new int[length];
-    A[0] = 301;
-    A[1] = 101;
-    A[2] = 4002;
-    A[3] = 3;
-    A[4] = 220;
-    A[5] = 111;
-    A[6] = 21;
-    A[7] = 5;
-    A[8] = 10000;
-    A[9] = 41;
+    std::random_device rnd;
 
-    //ソート前出力
+    //ソート前のデータを用意
+    A = new long[length];
+
     for (int i=0;i<length;i++) {
-        printf("%d\n",A[i]);
+        A[i] = (long)rnd();
     }
 
-    printf("-----------\n");
+    printf("Before data-----------\n\n");
+    //ソート前出力
+    for (int i = 0; i < length; i++) {
+        printf("%ld\n",A[i]);
+    }
+
+    printf("Sorted data-----------\n\n");
 
     //バブルソート処理実行
-    BubleSort(A,length);
+    BubleSort(A, length);
 
     //ソート後出力
-    for (int i=0;i<length;i++) {
-        printf("%d\n",A[i]);
+    for (int i = 0; i < length; i++) {
+        printf("%ld\n",A[i]);
     }
 
 
     return 0;
 }
 
-void BubleSort(int *A, int N) {
+void BubleSort(long *A, int N) {
 
     int i, j;
-    int temp;
+    long temp;
 
     for (int i = N - 1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
-            if (A[j] > A[j+1]) {
+            if (A[j] > A[j + 1]) {
                 temp = A[j];
-                A[j] = A[j+1];
-                A[j+1] = temp;
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
             }
         }
     }
